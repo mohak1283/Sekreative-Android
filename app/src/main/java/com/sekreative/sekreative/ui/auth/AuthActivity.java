fdd9f98f9a076afd8210ca77597cc7c812c4d58a
= <sriramr083@gmail.com>
2018-10-11 00:04:22 +0530

    removed signup and login fragment due to requirement of only one screen for phone auth

--- app/src/main/java/com/sekreative/sekreative/ui/auth/AuthActivity.java
+++ app/src/main/java/com/sekreative/sekreative/ui/auth/AuthActivity.java
@@ -1,14 +1,11 @@
 package com.sekreative.sekreative.ui.auth;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 
 import com.sekreative.sekreative.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -17,24 +14,6 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        if (savedInstanceState == null) {
-            showFragment(LoginFragment.instantiate());
-        }
-
-    }
-
-    private void showFragment(Fragment fragment) {
-        showFragment(fragment, "");
-    }
-
-    private void showFragment(Fragment fragment, String tag) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        if (!TextUtils.isEmpty(tag)) {
-            transaction.addToBackStack(tag);
-        }
-
-        transaction.replace(R.id.frame_auth, fragment);
     }
 
 }
